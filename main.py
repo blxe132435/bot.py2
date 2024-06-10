f937f2d8a4fe6666985616883be855b4d87b9a25
blxe132435 <159596543+blxe132435@users.noreply.github.com>
2024-06-10 12:19:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,38 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print('Bot {} telah online'.format(bot.user))
+    print("Bot ready")
+    synced = await bot.tree.sync()
+    print(f"(len{synced}) command(s)")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1239224281284481134)
+    text = f"Hello World, {member.mention}!"
+
+    emmbed = discord.Embed(title = 'Welcome to My World', description = text, color = 0x66ffff)
+
+    await channel.send(text)
+    await channel.send(embed = emmbed)
+    await member.send(text)
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1249552387476295780)
+    text = f"{member.name} has left the server!"
+    await channel.send(text)
+
+@bot.event
+async def on_message(message):
+    mes = message.content
+    if 'หวัดดี' in mes:
+        await message.channel.send("หวัดไม่ดีนะ")
+
+    elif 'บอท' in mes :
+        await message.channel.send("ผมบอทเองครับ" + str(message.author.name))
+
+    await bot.process_commands(message)
+
 
 @bot.command()
 async def hello(ctx):
